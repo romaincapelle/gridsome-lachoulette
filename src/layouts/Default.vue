a33e59e9a925a21f30bf52c11c9e7e864c43d3fd
romaincapelle <romain.capelle@gmail.com>
2020-05-17 16:11:51 -0700

    simple form ex

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="layout">
     <header class="header">
+      <g-image alt="Logo Choulette" src="~/favicon.png" width="35" />
+
       <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+        <h1>Bienvenue sur le drive de La Choulette</h1>
+        <p>
+          Du lundi au vendredi de 8h à 12h et de 14h à 18h + le samedi matin de
+          9h à 12h
+        </p>
       </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
     </header>
-    <slot/>
+    <hr />
+    <br />
+    <slot />
   </div>
 </template>
 
@@ -23,9 +27,10 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
@@ -46,5 +51,51 @@ body {
 
 .nav__link {
   margin-left: 20px;
+}
+
+.center {
+  text-align: center;
+}
+.legal {
+  margin: 40px auto;
+  padding: 15px;
+  font-size: 8px;
+  border: solid 1px black;
+  line-height: 15px;
+}
+
+input {
+  width: 100%;
+  padding: 7px 18px;
+  font-size: 15px;
+  margin-top: 5px;
+  margin-bottom: 28px;
+}
+
+button {
+  width: 100%;
+  padding: 20px;
+  background-color: #ebb716;
+  font-size: 17px;
+  color: #242424;
+  border-radius: 7 px;
+}
+
+button:hover {
+  width: 100%;
+  padding: 20px;
+  background-color: white;
+  font-size: 17px;
+  color: #ebb716;
+  border: 1px solid #242424;
+  cursor: pointer;
+}
+
+textarea {
+  width: 100%;
+  padding: 20px 18px;
+  font-size: 15px;
+  margin-top: 5px;
+  margin-bottom: 28px;
 }
 </style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,65 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h5>
+      Nous avons recu votre commande et nous vous attendons a la brasserie.
+    </h5>
+    <h4>16 Rue des Écoles 59111 Hordain</h4>
+    <p>Pour toute questions, veuillez nous contacter au +33 3 27 35 72 44</p>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
-  }
-}
+    title: 'About us',
+  },
+};
 </script>
+
+<style>
+.center {
+  text-align: center;
+}
+.legal {
+  margin: 40px auto;
+  padding: 15px;
+  font-size: 8px;
+  border: solid 1px black;
+  line-height: 15px;
+}
+
+input {
+  width: 100%;
+  padding: 7px 18px;
+  font-size: 15px;
+  margin-top: 5px;
+  margin-bottom: 28px;
+}
+
+button {
+  width: 100%;
+  padding: 20px;
+  background-color: #ebb716;
+  font-size: 17px;
+  color: #242424;
+  border-radius: 7 px;
+}
+
+button:hover {
+  width: 100%;
+  padding: 20px;
+  background-color: white;
+  font-size: 17px;
+  color: #ebb716;
+  border: 1px solid #242424;
+  cursor: pointer;
+}
+
+textarea {
+  width: 100%;
+  padding: 20px 18px;
+  font-size: 15px;
+  margin-top: 5px;
+  margin-bottom: 28px;
+}
+</style>
